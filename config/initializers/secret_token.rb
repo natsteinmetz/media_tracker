4ade180737d7b2c5a15876da8440300cf31c9355
natsteinmetz <nathalie.steinmetz@gmail.com>
2013-07-24 21:32:30 -0700

    Generate the media tracker application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MediaTracker::Application.config.secret_token = '0c8e8c5cb103da5ad121fdd81a2dbd69cc73028040c3bec210cc16e68b291f6b19d8ff7947ac3b26afcf4f0d6f3932c58d7a3ba07ec48088cdf76eae37b64b82'
